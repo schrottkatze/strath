1f59a4a874a3822331c163f3b33eb5547316fda4
Gabriel <68819302+obsidianical@users.noreply.github.com>
2021-12-16 09:03:40 +0100

    multiply strings by float

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!, {:#?}", env::args());
+    let args: Vec<String> = env::args().collect();
+    let file = fs::read_to_string(&args[1]).expect("Couldnt read file:");
+    println!("{:?}", multiply_strings(file, 3.5));
+}
+
+fn multiply_strings(str: String, multiplier: f32) -> String {
+    let mut r: String = String::from(str);
+    let mult_float = multiplier - (multiplier as i32) as f32;
+    let slice = &r.clone()[0..(r.len() as f32 * mult_float) as usize];
+
+    r = r.repeat(multiplier as usize);
+    r += slice;
+    r
 }
