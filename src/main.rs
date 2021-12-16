e2b1af59b4e287d1943ffb265069f5039324d534
Gabriel <68819302+obsidianical@users.noreply.github.com>
2021-12-16 09:40:17 +0100

    examples the first

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
 
 fn multiply_strings(str: String, multiplier: f32) -> String {
     let mut r: String = String::from(str);
-    let mult_float = multiplier - (multiplier as i32) as f32;
-    let slice = &r.clone()[0..(r.len() as f32 * mult_float) as usize];
+    let rest = multiplier - (multiplier as i32) as f32;
+    let slice = &r.clone()[0..(r.len() as f32 * rest) as usize];
 
     r = r.repeat(multiplier as usize);
     r += slice;
